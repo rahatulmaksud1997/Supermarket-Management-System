a491aede801fb5c7ab61fde11fe0b5efefe828ea
@rahatulmaksud1997 <ratulmaksud@gmail.com>
2024-02-05 11:31:27 +0600

    first commit

---
+++ WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Edit([FromRoute]int? id)
+        {
+            var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
